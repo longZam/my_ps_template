bb91c36cf8b86a278b245ae7dd37a08464bff24f
longZam <longZam305@gmail.com>
2023-05-11 11:12:50 +0900

    main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#pragma region Libraries
+
+
+
+#pragma region 거의 항상 사용할 수 있는 라이브러리
+
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <memory.h>
+
+#pragma endregion
+
+
+#pragma region 경우에 따라 금지당할 수 있는 라이브러리
+
+// 높은 확률로 금지될 수 있음
+#include <algorithm>
+
+// 간혹 금지될 수 있음, 직접 구현하는 자료구조 수업에서는 당연히 금지
+#include <stack>
+#include <queue>
+#include <list>
+#include <vector>
+#include <deque>
+#include <set> // 중복 체크용 해시 자료구조
+#include <map> // Key-Value 쌍을 가지는 해시 자료구조
+
+#pragma endregion
+
+
+
+#pragma endregion
+
+
+
+int main(int argc, char* argv[])
+{
+    int numTestCases;
+    std::cin >> numTestCases;
+
+    for (int _ = 0; _ < numTestCases; _++)
+    {
+
+    }
+}
